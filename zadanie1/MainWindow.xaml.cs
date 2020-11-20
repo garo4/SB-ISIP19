a1ebac7a521dc3e3acc6816e23fc763142ad669f
Nikita Sleptsov <nik.sleptsov56@mail.ru>
2020-11-20 12:00:44 +0900

    first commit

---
+++ zadanie1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zadanie1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, RoutedEventArgs e)
+        {
+            double x1, x2, y, z;
+            x1 = Convert.ToDouble(Max.Text);
+            x2 = Convert.ToDouble(Min.Text);
+            z = Convert.ToDouble(s.Text);
+
+            while (x2 <= x1)
+            {
+                y = -0.23 * Math.Pow(x2, 2) + x2;
+                answer.Text += Convert.ToString("x= " + x2 + "   " + "y= " + y + "\n");
+                x2 += z;
+            }
+        }
+    }
+}
